dfde19b2ecd11bc9762d97aeb1d3e41b986a31ef
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-03 10:53:05 +0200

    Setup basic GitHub Oauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <main>
-      Hello world!
+      Hello world! {JSON.stringify(session)}
+      {!session ? (
+        <>
+          <h1>Sign in</h1>
+          <form
+            action={async () => {
+              "use server";
+              await signIn();
+            }}
+          >
+            <button type="submit">Signin</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1>Sign out</h1>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button type="submit">Signout</button>
+          </form>
+        </>
+      )}
     </main>
   );
 }
