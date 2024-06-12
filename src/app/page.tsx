a6b16bc14fd24fb16bed1f0f153874684c43b121
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-12 13:21:47 +0200

    Remove next-auth and start using fireauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,21 @@
-import { auth, signIn, signOut } from "@/auth";
 import Link from "next/link";
 
 export default async function Home() {
-  const session = await auth();
-
   return (
     <main className={"flex flex-col items-center gap-10"}>
       <h1 className={"text-3xl mt-20"}>Home</h1>
-      {JSON.stringify(session)}
-      {!session ? (
-        <form
-          action={async () => {
-            "use server";
-            await signIn();
-          }}
-        >
-          <button
-            className={"bg-black px-3 py-2 text-lg text-white rounded-md"}
-            type="submit"
-          >
-            Sign in
-          </button>
-        </form>
-      ) : (
-        <form
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
-        >
-          <button
-            className={"bg-black px-3 py-2 text-lg text-white rounded-md"}
-            type="submit"
-          >
-            Sign out
-          </button>
-        </form>
-      )}
+      <button
+        className={"bg-black px-3 py-2 text-lg text-white rounded-md"}
+        type="submit"
+      >
+        Sign in
+      </button>
+      <button
+        className={"bg-black px-3 py-2 text-lg text-white rounded-md"}
+        type="submit"
+      >
+        Sign out
+      </button>
       <Link href={"/profile"}>Profile</Link>
     </main>
   );
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,10 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 import Link from "next/link";
 
 export default async function Profile() {
-  const session = await auth();
-
-  if (!session) {
-    return redirect("/api/auth/signin?callbackUrl=/profile");
-  }
-
   return (
     <main className={"flex flex-col items-center gap-10 p-10"}>
       <h1 className={"text-3xl mt-20"}>Profile page</h1>
-      <p>This is a protected page. You are signed in as {session.user.name}</p>
+      <p>This is a protected page. You are signed in as</p>
       <Link href={"/"}>Home</Link>
     </main>
   );
