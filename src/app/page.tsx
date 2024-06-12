1f11c1a81c73fe2ab5fab1a53c652ce1c8fbe203
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-12 13:21:47 +0200

    Remove next-auth and start using fireauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import Posts from "@/components/posts/Posts";
-import PostForm from "@/components/posts/PostForm";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Homepage</h1>
-      <PostForm />
-      <Posts />
+    <main className={"flex flex-col items-center gap-10"}>
+      <h1 className={"text-3xl mt-20"}>Home</h1>
+      <button
+        className={"bg-black px-3 py-2 text-lg text-white rounded-md"}
+        type="submit"
+      >
+        Sign in
+      </button>
+      <button
+        className={"bg-black px-3 py-2 text-lg text-white rounded-md"}
+        type="submit"
+      >
+        Sign out
+      </button>
+      <Link href={"/profile"}>Profile</Link>
     </main>
   );
 }
