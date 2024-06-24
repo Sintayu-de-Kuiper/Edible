2f01a7c564aa3b7a23d1b08e8804a1f76829e7bf
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-24 13:54:11 +0200

    Create a logout button for the profle page

--- src/components/CurrentUserProfile.tsx
+++ src/components/CurrentUserProfile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAuth } from "@/hooks/useAuth";
-import { googleSignIn } from "@/lib/auth";
+import { googleSignIn, signOut } from "@/lib/auth";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -22,28 +22,41 @@ export function CurrentUserProfile() {
   }
 
   return (
-    <div className="min-h-screen">
-      <div className="max-w-4xl mx-auto p-4">
-        <div className="flex flex-col items-center">
-          <Image
-            src={user.photoURL ?? "/default-avatar.svg"}
-            alt="Profile picture"
-            width={128}
-            height={128}
-            className="rounded-full"
-          />
-          <div className="mt-5 flex flex-col items-center">
-            <h1 className="text-2xl font-bold">{user.displayName}</h1>
-          </div>
-          <div className="flex flex-col items-center mt-4">
-            <div className="flex space-x-8 mt-4">
-              <p>
-                <span className="font-bold">(number)</span> Following
-              </p>
-              <p>
-                <span className="font-bold">(number)</span> Follower
-              </p>
-            </div>
+    <div className="relative min-h-screen max-w-4xl mx-auto p-4">
+      <button
+        onClick={signOut}
+        className="absolute top-4 right-4 p-2 text-destructive"
+        aria-label="Logout"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor"
+        >
+          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+        </svg>
+      </button>
+      <div className="flex flex-col items-center">
+        <Image
+          src={user.photoURL ?? "/default-avatar.svg"}
+          alt="Profile picture"
+          width={128}
+          height={128}
+          className="rounded-full"
+        />
+        <div className="mt-5 flex flex-col items-center">
+          <h1 className="text-2xl font-bold">{user.displayName}</h1>
+        </div>
+        <div className="flex flex-col items-center mt-4">
+          <div className="flex space-x-8 mt-4">
+            <p>
+              <span className="font-bold">(number)</span> Following
+            </p>
+            <p>
+              <span className="font-bold">(number)</span> Follower
+            </p>
           </div>
         </div>
       </div>
