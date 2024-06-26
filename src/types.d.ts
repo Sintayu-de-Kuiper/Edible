d5050c6599b54afd7074b6f349d267e5151dcbba
Rutger <1201954@student.roc-nijmegen.nl>
2024-06-26 14:05:48 +0200

    Add post types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,31 @@
-export interface Post {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
-}
-
 export interface User {
   id: string;
   name: string;
   email: string;
   imageUrl: string;
+  createdAt: Date;
+}
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+  imageUrl: string;
+  userId: string;
+  createdAt: Date;
+  likes: Like[];
+  comments: Comment[];
+}
+
+export interface Like {
+  id: string;
+  userId: string;
+  likedAt: Date;
+}
+
+export interface Comment {
+  id: string;
+  userId: string;
+  content: string;
   createdAt: Date;
 }
